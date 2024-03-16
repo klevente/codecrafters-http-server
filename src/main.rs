791f0a6a87e399d2f457f22c0b062a6301f730fb
Levente Krizsan <lewalevi@gmail.com>
2024-03-16 16:44:20 +0100

    respond with empty 200

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
+use std::io::Write;
 use std::net::TcpListener;
+use anyhow::Context;
 
-fn main() {
+
+// HTTP/1.1 200 OK\r\n\r\n
+fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                // stream.write_fmt()
+                write!(&mut stream, "HTTP/1.1 200 OK\r\n\r\n").context("sending TCP response")?;
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("error: {e}");
             }
         }
     }
+
+    Ok(())
 }
