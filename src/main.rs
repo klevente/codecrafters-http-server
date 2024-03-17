126be6b3ffad55c426bb8d1aaa8eba289e3a5499
Levente Krizsan <lewalevi@gmail.com>
2024-03-17 13:35:47 +0100

    steamline path handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Context;
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 /*
 GET /echo/abc HTTP/1.1
@@ -8,46 +8,80 @@ Host: localhost:4221
 User-Agent: curl/7.64.1
  */
 fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:4221").context("opening socket")?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-
-                let mut request = [0; 1024];
-                stream.read(&mut request).context("reading request")?;
-
-                let request = String::from_utf8_lossy(&request);
-
-                println!("read data");
-
-                let Some((start_line, _)) = request.split_once("\r\n") else {
-                    anyhow::bail!("request doesn't have a newline");
-                };
-
-                let Some(path) = start_line.splitn(3, ' ').nth(1) else {
-                    anyhow::bail!("request doesn't have a path");
-                };
-
-                let Some((_, sub_path)) = path.rsplit_once('/') else {
-                    anyhow::bail!("request doesn't match expected value");
-                };
-
-                let content_length = sub_path.len();
-
-                write!(
-                    &mut stream,
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{sub_path}"
-                )
-                .context("sending TCP response")?;
-                println!("after response");
+            Ok(stream) => {
+                handle_request(stream)?;
             }
             Err(e) => {
-                println!("error: {e}");
+                println!("error occurred during setting up the connection: {e}");
             }
         }
     }
 
     Ok(())
+}
+
+fn handle_request(mut stream: TcpStream) -> anyhow::Result<()> {
+    println!("accepted new connection");
+
+    let mut request = [0; 1024];
+    stream.read(&mut request).context("reading request")?;
+
+    let request = String::from_utf8_lossy(&request);
+
+    println!("read data");
+
+    let Some((start_line, _)) = request.split_once("\r\n") else {
+        anyhow::bail!("request doesn't have a newline");
+    };
+
+    let Some(path) = start_line.splitn(3, ' ').nth(1) else {
+        anyhow::bail!("request doesn't have a path");
+    };
+
+    let Some(path_without_slash) = path.strip_prefix('/') else {
+        anyhow::bail!("path doesn't start with '/'");
+    };
+
+    if path_without_slash.is_empty() {
+        write_response(&mut stream, 200, &[], None)?;
+        return Ok(());
+    }
+
+    if let Some(sub_path) = path.strip_prefix("echo/") {
+        let content_length = sub_path.len().to_string();
+
+        write_response(
+            &mut stream,
+            200,
+            &[
+                ("Content-Type", "text/plain"),
+                ("Content-Length", &content_length),
+            ],
+            Some(sub_path),
+        )?;
+    } else {
+        write_response(&mut stream, 404, &[], None)?;
+    }
+
+    println!("successfully handled request");
+
+    Ok(())
+}
+
+fn write_response(
+    stream: &mut TcpStream,
+    status_code: u16,
+    headers: &[(&str, &str)],
+    body: Option<&str>,
+) -> anyhow::Result<()> {
+    let headers = headers
+        .iter()
+        .fold(String::new(), |acc, (k, v)| acc + &format!("{k}: {v}\r\n"));
+    let body = body.unwrap_or("");
+    write!(stream, "HTTP/1.1 {status_code}\r\n{headers}\r\n{body}")
+        .context("writing response to stream")
 }
